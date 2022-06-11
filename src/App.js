d4bf0792954bcf62d94e717ac1bc82404c746dcd
Arch0wl <annstaroviotov@gmail.com>
2022-06-11 09:37:49 -0400

    adding assets

--- src/App.js
+++ src/App.js
@@ -1,46 +1,15 @@
+import React from "react";
 import "./App.css";
-import { useState, useEffect } from "react";
-import { storage } from "./firebase";
-import { ref, uploadBytes, listAll, getDownloadURL } from "firebase/storage";
-import { v4 } from "uuid"; // uuuid library for different files types
+import UploadIm from "../src/components/UploadImages/UploadIm";
+// import { useState, useEffect } from "react";
+// import { storage } from "./firebase";
+// import { ref, uploadBytes, listAll, getDownloadURL } from "firebase/storage";
+// import { v4 } from "uuid"; // uuuid library for different files types
 
 function App() {
-  const [imageUpload, setImageUpload] = useState(null);
-  const [imageList, setImageList] = useState([]);
-  const imaListRef = ref(storage, "images/");
-
-  const uploadImage = () => {
-    if (imageUpload == null) return;
-    const imageRef = ref(storage, `images/${imageUpload.name + v4()}`);
-    uploadBytes(imageRef, imageUpload).then((snaphsot) => {
-      getDownloadURL(snaphsot.ref).then((url) => {
-        setImageList((prev) => [...prev, url]);
-      });
-    });
-  };
-
-  useEffect(() => {
-    listAll(imaListRef).then((response) => {
-      response.items.forEach((item) => {
-        getDownloadURL(item).then((url) => {
-          setImageList((prev) => [...prev, url]);
-        });
-      });
-    });
-  }, [imaListRef]);
-
   return (
     <div className="App">
-      <input
-        type="file"
-        onChange={(event) => {
-          setImageUpload(event.target.files[0]);
-        }}
-      />
-      <button onClick={uploadImage}>Upload Image</button>
-      {imageList.map((url) => {
-        return <img src={url} alt="" />;
-      })}
+      <UploadIm />
     </div>
   );
 }
