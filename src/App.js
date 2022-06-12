f9b0baacab17e301507c1c9c9981688c2906c008
Arch0wl <annstaroviotov@gmail.com>
2022-06-12 17:42:05 -0400

    adding style

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import { Modal } from "@material-ui/core";
 import React from "react";
 // import "./App.css";
 import UploadIm from "../src/components/UploadImages/UploadIm";
 import Gallery from "./components/UploadImages/Gellery";
+import ImageGrid from "./components/UploadImages/ImageGrid";
 // import Menubar from "./components/common/MenuBar";
+
 // import { useState, useEffect } from "react";
 // import { storage } from "./firebase";
 // import { ref, uploadBytes, listAll, getDownloadURL } from "firebase/storage";
@@ -12,10 +15,16 @@ function App() {
   return (
     <>
       <UploadIm />
-      <Gallery kidId="h0X9JLF98v2wfZGlCA71" />
+      <Gallery />
+      <ImageGrid />
+      <Modal />
       {/* <Menubar /> */}
     </>
   );
 }
 
 export default App;
+
+{
+  /* <Gallery kidId="h0X9JLF98v2wfZGlCA71" /> */
+}
